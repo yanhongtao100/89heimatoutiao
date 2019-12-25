1f63b73b63f845c61312f4b51efc8c5a71a74a61
yanhongtao100 <602503398@qq.com>
2019-12-25 16:04:08 +0800

    拉取前的代码

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,10 @@ const routes = [
     {
       path: 'articles',
       component: () => import('../views/articles')
+    },
+    {
+      path: 'publish',
+      component: () => import('../views/publish')
     }]
   },
   {
